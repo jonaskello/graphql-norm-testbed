ddcd1274a56e87fc0230869b7e8253b30c976858
Jonas Kello <jonas.kello@gmail.com>
2020-01-01 13:16:37 +0100

    Refactor

--- src/denorm.js
+++ src/denorm.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
+const path = require("path");
 const { denormalize } = require("graphql-norm");
 const { parse } = require("graphql");
 
-const query = require("./denorm-query");
+const queryText = fs.readFileSync(
+  path.join(__dirname, "./denorm-query.graphql"),
+  "utf8"
+);
+
 const cache = require("./denorm-cache.json");
 
-const queryDoc = parse(query);
+const queryDoc = parse(queryText);
 const denormResult = denormalize(queryDoc, {}, cache);
 
 const setToJSON = (k, v) => (v instanceof Set ? Array.from(v) : v);
 console.log("denormResult", JSON.stringify(denormResult, setToJSON));
 
 const data = JSON.stringify(denormResult.data);
-fs.writeFileSync("./denorm-result-data.json", data);
+fs.writeFileSync(path.join(__dirname, "./denorm-result-data.json"), data);
